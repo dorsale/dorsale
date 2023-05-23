b4c245913fded19ac99ae46d7f671011a6d5a382
Joel <joel@dibasso.fr>
2023-05-23 23:55:35 +0200

    Can bind query parameters

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,11 @@
-import { Controller, Get } from "./decorators";
+import { Controller, Get, Query } from "./decorators";
 
 @Controller
 export class TestController {
   id: number = 0;
 
   @Get("/test/:n")
-  test(n: string) {
-    return 3 + parseInt(n);
+  test(@Query m: string, @Query p: string, n: string) {
+    return 3 + parseInt(n) + parseInt(m) - parseInt(p);
   }
 }
