27f181eb8070d432d0c113566b28935054403809
Joel <joel@dibasso.fr>
2023-05-25 23:36:37 +0200

    Make project into a real package

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-import { dorsal } from "./server";
-
-dorsal()
-  .then((app) => {
-    app.listen({ port: 3000 }, (err, address) => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      console.log(`Server listening at ${address}`);
-    });
-  });
+export { dorsal } from "./server";
+export { Controller, Get, Query } from "./decorators";
